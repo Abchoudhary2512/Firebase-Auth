24760162d465b7477c7002c301c00903771c6045
Ajay <bhakarajay033@gmail.com>
2023-07-21 13:17:05 +0530

    second commit

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,23 +1,4 @@
-// import React, { useState } from "react";
-// import Signin from "./signin";
-// import Signup from "./signup";
 
-// const Auth = () => {
-//   const [index, setIndex] = useState(false);
-//   const toggleIndex = () => {
-//     setIndex((prevState) => !prevState);
-//   };
-//   return (
-//     <div className="container">
-//       {!index ? <Signin /> : <Signup />}
-//       <p onClick={toggleIndex}>
-//         {!index ? "New user? Click here " : "Already have an acount?"}
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default Auth;
 import React from "react";
 import { useUserContext } from "../context/userContext";
 
